08e9433f94f84f3933e267b64df4077b4c658448
darwin mamani <darwinjr40@gmail.com>
2021-10-01 21:07:43 -0400

    primer commit

---
+++ src/Negocio/numero.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+/**
+ *
+ * @author darwin
+ */
+public class numero {
+    int n;
+
+    public numero(int n) {
+        this.n = n;
+    }
+    
+    
+          
+}
